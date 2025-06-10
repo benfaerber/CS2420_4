2d3292b30a36b0c716f020346236d7e944abd18b
Ben Faerber <benfaerber@protonmail.com>
2025-06-10 10:04:28 -0600

    Start ListSorter

--- src/assign05/ListSorter.java
+++ src/assign05/ListSorter.java
@@ -5,13 +5,12 @@ import java.util.Comparator;
 import java.util.List;
 
 public class ListSorter {
-
     public static <T extends Comparable<? super T>> void insertionSort(List<T> list) {
         for (int i = 0; i < list.size(); i++) {
             for (int j = i; j > 0; j--) {
                 T currentUnsortStar = list.get(j);
                 T currentUnsortEnd = list.get(j - 1);
-                if (currentUnsortStar.compareTo(currentUnsortEnd) > 0) {
+                if (currentUnsortStar.compareTo(currentUnsortEnd) < 0) {
                     T temp = list.get(j);
                     list.set(j, currentUnsortEnd);
                     list.set(j-1, temp);
@@ -27,26 +26,67 @@ public class ListSorter {
 
         boolean shouldInsert = list.size() <= threshold;
         if (shouldInsert) {
+            System.out.println("Using Insertion Sort");
             insertionSort(list);
             return;
         }
-
         mergesortRec(list, 0, list.size() - 1);
     }
 
     private static <T extends Comparable<? super T>> void mergesortRec(List<T> list, int lower, int upper) {
-        List<T> auxStorage = new ArrayList<T>();
+        // Base Cases
+        if (lower == upper) {
+            return;
+        }
 
         int middle = (lower + upper) / 2;
 
         mergesortRec(list, lower, middle);
         mergesortRec(list, middle + 1, upper);
 
-        // merge(list, lower, middle, upper);
+        merge(list, lower, middle, upper);
     }
 
     private static <T extends Comparable<? super T>> void merge(List<T> list, int lower, int middle, int upper) {
+        int leftLength = middle - lower + 1;
+        int rightLength  = upper - middle;
 
+        List<T> leftSide = new ArrayList<T>(leftLength);
+        List<T> rightSide = new ArrayList<T>(rightLength);
+
+        for (int i = 0; i < leftLength; i++) {
+            leftSide.add(list.get(lower + i));
+        }
+        for (int i = 0; i < rightLength; i++) {
+            rightSide.add(list.get(middle + 1 + i));
+        }
+
+        int i = 0, j = 0;
+
+        int currentIndex = lower;
+        while (i < leftLength && j < rightLength) {
+            int leftVsRightCmp = leftSide.get(i).compareTo(rightSide.get(j));
+            if (leftVsRightCmp <= 0) {
+                list.set(currentIndex, leftSide.get(i));
+                i++;
+            } else {
+                list.set(currentIndex, rightSide.get(j));
+                j++;
+            }
+            currentIndex++;
+        }
+
+        while (i < leftLength) {
+            list.set(currentIndex, leftSide.get(i));
+            i++;
+            currentIndex++;
+        }
+
+        while (j < rightLength) {
+            list.set(currentIndex, rightSide.get(j));
+            j++;
+            currentIndex++;
+        }
     }
 
     public static <T extends Comparable<? super T>> void quicksort(List<T> list, PivotChooser<T> chooser) {
--- src/assign05/ListSorterTest.java
+++ src/assign05/ListSorterTest.java
@@ -38,14 +38,41 @@ public class ListSorterTest {
         return lst;
     }
 
+    private static <T> void printArrayList(String label, ArrayList<T> lst) {
+        System.out.println(label + ":");
+        for (T item : lst) {
+            System.out.print(item + ", ");
+        }
+    }
+
     @Test
     void testMergeSort() {
+        // Test without caring about the insertion sort stuff
         ArrayList<Integer> lst = createArrayList(new Integer[] {1, 4, 5, 3, 2});
         ArrayList<Integer> expected = createArrayList(new Integer[] {1, 2, 3, 4, 5});
-        ListSorter.insertionSort(lst);
-        ListSorter.mergesort(lst, 1);
+
+        ListSorter.mergesort(lst, 2);
+        printArrayList("MergeSort", lst);
+
 
         assertArrayEquals(expected.toArray(), lst.toArray());
+    }
+
+    @Test
+    void testInsertionSort() {
+        ArrayList<Integer> lst = createArrayList(new Integer[] {1, 4, 5, 3, 2});
+        ArrayList<Integer> expected = createArrayList(new Integer[] {1, 2, 3, 4, 5});
+
+        ListSorter.insertionSort(lst);
+        assertArrayEquals(expected.toArray(), lst.toArray());
+    }
+
+    @Test
+    void testUnderThreshold() {
+        ArrayList<Integer> lst = createArrayList(new Integer[] {1, 4, 5, 3, 2});
+        ArrayList<Integer> expected = createArrayList(new Integer[] {1, 2, 3, 4, 5});
+
+        ListSorter.mergesort(lst, 10);
         assertArrayEquals(expected.toArray(), lst.toArray());
     }
 }
