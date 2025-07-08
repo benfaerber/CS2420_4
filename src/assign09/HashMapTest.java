6aa47d047dce5a7adec3e83613e45fe30aa93d1c
Ben Faerber <benfaerber@protonmail.com>
2025-07-07 18:49:14 -0600

    Stuff

---
+++ src/assign09/HashMapTest.java
@@ -0,0 +1,18 @@
+package assign09;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class HashMapTest {
+    @BeforeEach
+    void setUp() throws Exception {
+    }
+
+    @AfterEach
+    void tearDown() throws Exception {}
+}
+
+
+
+
---
+++ src/assign09/HashTable.java
@@ -0,0 +1,61 @@
+package assign09;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HashTable<K, V> implements Map<K, V> {
+    private static final int DEFAULT_CAPACITY = 101;
+    private static final int[] PRIMES = new int[] {
+            101, 211, 431, 863, 1733, 3467, 6947, 13901, 27803,
+            55609, 111227, 222461, 444929, 889871, 1779761
+    };
+
+    private ArrayList<MapEntry<K, V>> table;
+
+    public HashTable() {
+        table = this.createTableWithCapacity(DEFAULT_CAPACITY);
+    }
+
+    private ArrayList<MapEntry<K, V>> createTableWithCapacity(int capacity) {
+        table = new ArrayList<>();
+        for(int i = 0; i < capacity; i++)
+            table.add(null);
+        return table;
+    }
+
+    public void clear() {
+        this.table = this.createTableWithCapacity(DEFAULT_CAPACITY);
+    }
+
+    public boolean containsKey(K key) {
+        throw new UnsupportedOperationException();
+    }
+
+    public boolean containsValue(V value) {
+        throw new UnsupportedOperationException();
+    }
+
+    public List<MapEntry<K, V>> entries() {
+        throw new UnsupportedOperationException();
+    }
+
+    public V get(K key) {
+        throw new UnsupportedOperationException();
+    }
+
+    public boolean isEmpty() {
+        throw new UnsupportedOperationException();
+    }
+
+    public V put(K key, V value) {
+        throw new UnsupportedOperationException();
+    }
+
+    public V remove(K key) {
+        throw new UnsupportedOperationException();
+    }
+
+    public int size() {
+        throw new UnsupportedOperationException();
+    }
+}
